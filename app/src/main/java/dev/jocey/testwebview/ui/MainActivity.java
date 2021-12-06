407826f3dbea0e9b01975efed119180ec20a62fc
Oleksandr Holynkov <golinkovsasha@gmail.com>
2021-12-06 23:16:18 +0200

    Initial commit

---
+++ app/src/main/java/dev/jocey/testwebview/ui/MainActivity.java
@@ -0,0 +1,18 @@
+package dev.jocey.testwebview.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dev.jocey.testwebview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+
+}
